953fd05a3b2a089ea66be31b90d18daa7bcf1dc2
Janith-max <151737728+Janith-max@users.noreply.github.com>
2023-12-08 02:52:55 +0530

    Create alive.js

---
+++ Adam/Database/alive.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Alive = new mongoose.Schema({
+  id: { type: String,  unique: true ,required: true, default:"1"},
+  text: { type: String, default:"Im Online , How can i help you" },
+  get: { type: String, default:"I am adam x bot , How can i help you"},
+  url: { type: String, default:""},
+  image: { type: Boolean, default: false },
+  video: { type: Boolean, default: false }
+});
+
+const alive =mongoose.model("alive", Alive)
+module.exports = { alive }
