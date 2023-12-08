882f1d0c20ef3d0d5fc034db1990916217dc7555
Janith-max <151737728+Janith-max@users.noreply.github.com>
2023-12-08 15:05:55 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,66 @@
+const fs = require('fs-extra')
+if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
+
+
+//═══════[Required Variables]════════\\
+global.owner = process.env.OWNER_NUMBER.split("94752013704,")
+global.mongodb = process.env.MONGODB_URI || "mongodb://mongo:cCBe2fBeA26f4eEbDbC2gD64226EHFg3@viaduct.proxy.rlwy.net:34483"
+global.port= process.env.PORT || 5000
+global.email = 'janithidunil34@.live'
+global.blockJids = process.env.BLOCK_JID ||'120363165030810524@g.us' ;
+global.allowJids = process.env.ALLOW_JID ||'120363165030810524@g.us' ;
+global.github = 'https://github.com/Janith-max/ADAM-BOT'
+global.location = 'Sri Lanka'
+global.gurl = 'https://instagram.com/' // add your username
+global.sudo = process.env.SUDO || '94752013704'
+global.devs = '94752013704';
+global.INBOX_BLOCK = 'true'
+global.TIMEZONE = 'Asia/Colombo' 
+global.website = 'https://github.com/Janith-max/ADAM-BOT' //wa.me/+91000000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.ibb.co/Xy30DrV/Picsart-23-12-08-03-30-18-081.jpg'
+module.exports = {
+  botname:   process.env.BOT_NAME === undefined ? 'ADAM-xBOT-MD' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'ADAM-xJANETH' : process.env.OWNER_NAME,
+  ownernumber: process.env.OWNER_NUMBER === undefined ? '94752013704' : process.env.OWNER_NUMBER,
+  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'ADAM xBOT GITHUB:JANETH : process.env.PACK_INFO.split(";")[0],
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'ᴀᴅᴀᴍ x ʙᴏᴛ' : process.env.PACK_INFO.split(";")[1],	
+  dl_limit: process.env.DL_SIZE || '350',
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
+  antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
+  antibadword : process.env.ANTI_BAD_WORD || 'pornhub',
+  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '212' : process.env.FAKE_COUNTRY_CODE,
+  readmessage:  process.env.READ_MESSAGE === undefined ? true : process.env.READ_MESSAGE,
+  HANDLERS:  process.env.PREFIX === undefined ? ['#','!','.'] : process.env.PREFIX,
+  warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
+  disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
+  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
+  antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
+  antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
+  BRANCH: 'main',
+  autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
+  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
+  caption :process.env.CAPTION || "  \t🎧 *ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀᴅᴀᴍ x ᴊᴀɴᴇᴛʜ*",
+  OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
+  heroku:  process.env.heroku === undefined ? false : process.env.heroku,
+  HEROKU: {
+    HEROKU: process.env.HEROKU ||false,
+    API_KEY: process.env.HEROKU_API_KEY === undefined ? '1abfce1e-1bee-4334-9f6c-f4c1cb1cafab' : process.env.HEROKU_API_KEY,
+    APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'zeropgg' : process.env.HEROKU_APP_NAME
+},
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+  LANG: process.env.THEME|| 'ADAM',
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+};
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+    delete require.cache[file]
+	require(file)
+})
