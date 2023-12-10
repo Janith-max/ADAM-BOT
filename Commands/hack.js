e94f7c95e0fb06ef2533aa4516234e54c57d8d1e
Janith-max <151737728+Janith-max@users.noreply.github.com>
2023-12-10 14:39:45 +0530

    Create hack.js

---
+++ Commands/hack.js
@@ -0,0 +1,105 @@
+const {fetchJson,cmd, tlang,sleep } = require('../lib')
+
+//---------------------------------------------------------------------------
+
+cmd({
+
+            pattern: "hack",
+
+            category: "prank",
+
+            desc: "hacked completed",
+
+            use: ' ',
+
+            filename: __filename,
+
+        },
+
+        async(Void,citel, text) => {
+
+citel.reply("```Injecting divice```")
+
+await sleep(1000)
+
+citel.reply("```linking into device \n 0%```")
+
+await sleep(1000)
+
+citel.reply("```transfering photos termux \n █ 10%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ 20%```")
+
+await sleep(1000)
+
+citel.reply("```transfering videos \n █ █ █ 30%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ 40%```")
+
+await sleep(1000)
+
+citel.reply("```transfering audio \n █ █ █ █ █ 50%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ █ █ 60%```")
+
+await sleep(1000)
+
+citel.reply("```transfering hidden files \n █ █ █ █ █ █ █ 70%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ █ █ █ █ 80%```")
+
+await sleep(1000)
+
+citel.reply("```transfering inbox  chat \n █ █ █ █ █ █ █ █ █ 90%```")
+
+await sleep(1000)
+
+citel.reply("```transfering group chat \n █ █ █ █ █ █ █ █ █ 90%```")
+
+await sleep(1000)
+
+citel.reply("```transfering hidden whatsapp chat \n █ █ █ █ █ █ █ █ █ 90%```")
+
+await sleep(1000)
+
+citel.reply("```calculate successful \n █ █ █ █ █ █ █ █ █ █ 100%```")
+
+await sleep(1000)
+
+citel.reply("```Https system type Linking on process.. \n Conecting to Server ```")
+
+await sleep(1000)
+
+citel.reply("```Divice successfully Linked... \n Riciving data...```")
+
+await sleep(1000)
+
+citel.reply("```Data linked from divice 100% completed \n Sending a Number banned mail...```")
+
+await sleep(1000)
+
+citel.reply("```Banned mail sent successfully```")
+
+await sleep(1000)
+
+citel.reply("``` Sending your wrong details ...```")
+
+await sleep(1000)
+
+citel.reply("``` successfully number linking project. Tunnel cnnection secure```")
+
+await sleep(1000)
+
+            return citel.reply('*Am linked your tunnel projects. guy*');
+
+        }
+
+    )
